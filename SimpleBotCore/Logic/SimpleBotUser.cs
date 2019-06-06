b352acf9fcbaf6930785d11fdc6f9f865c5fe6a6
Luan Goncalves <luangoncalves239@gmail.com>
2019-06-05 22:31:07 -0300

    alteracoes

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace SimpleBotCore.Logic
 {
-    public class SimpleBotUser
+      public class SimpleBotUser
     {
+        
         public string Reply(SimpleMessage message)
         {
-            return $"{message.User} disse '{message.Text}'";
-            //
+            SalvarMsg(message);
+
+            return $"{message.User} disse '{message.Text}' (mensagens enviadas)";
+
+        }
+
+        public void SalvarMsg(SimpleMessage message)
+        {
+            var cliente = new MongoClient("mongodb://localhost:27017");
+
+            var db = cliente.GetDatabase("DesafioSalvarHistorico");
+
+            var col = db.GetCollection<BsonDocument>("colMensagem");
+
+            var doc = new BsonDocument() {
+                { "Usuario", message.User },
+                { "Mensagem", message.Text }
+            };
+
+            col.InsertOne(doc);
+
+        }
+
+
+        public void Consultar(SimpleMessage message)
+        {
+
+
+
         }
 
     }
